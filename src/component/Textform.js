73b1a4eadae7d8cca0784f87cbf8ac1a1b803912
chetan09upadhyay <chetanupadhyay653@gmail.com>
2023-08-30 10:07:10 +0530

    Update Textform.js

--- src/component/Textform.js
+++ src/component/Textform.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+ import React, { useState } from 'react'
 
 
 export default function Textform(props) {
@@ -50,12 +50,13 @@ export default function Textform(props) {
 
         }
         setText(newtext)
-        // props.showAlert("Converted to alternate string","success");
+         props.showAlert("Converted to alternate string","success");
     }
 
     const handleExtraSpaces = () =>{
         let newText = text.split(/[ ]+/);
-        setText(newText.join(" "))
+        setText(newText.join(" "));
+        props.showAlert("Removed extraspaces","success");
     }
 
     const [text, setText] = useState('Enter text here');
@@ -65,25 +66,25 @@ export default function Textform(props) {
     return (
         <>
             <div className="container" style={{color: props.mode === 'dark'?'white':'#042743'}}>
-                <h1 >{props.heading}</h1>
+                <h1  >{props.heading}</h1>
                 <div className="mb-3"  >
                     <textarea className="form-control" value={text} onChange={handleOnChange} 
-                    style={{backgroundColor: props.mode === 'dark'?'grey':'white',color: props.mode === 'dark'?'white':'#042743'}}
+                    style={{backgroundColor: props.mode === 'dark'?'#13466e':'white',color: props.mode === 'dark'?'white':'#042743'}}
                      id=" myBox" rows="8" ></textarea>
                 </div>
-                <button className="btn btn-primary mx-2 my-1" onClick={handleUpClick} >Convert to upercase</button>
-                <button className="btn btn-danger mx-2 my-1" onClick={handleLoClick} >Convert to lowercase</button>
-                <button className="btn btn-success mx-2 my-1" onClick={onAlternatingCase} >Alternate change Text</button>
-                <button className="btn btn-success mx-2 my-1" onClick={handleExtraSpaces} >Remove extraspaces</button>
-                <button className="btn btn-danger mx-2 my-1" onClick={handlelearClick} >Clear Text</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleUpClick} >Convert to upercase</button>
+                <button disabled={text.length===0} className="btn btn-danger mx-2 my-1" onClick={handleLoClick} >Convert to lowercase</button>
+                <button disabled={text.length===0} className="btn btn-success mx-2 my-1" onClick={onAlternatingCase} >Alternate change Text</button>
+                <button disabled={text.length===0} className="btn btn-success mx-2 my-1" onClick={handleExtraSpaces} >Remove extraspaces</button>
+                <button disabled={text.length===0} className="btn btn-danger mx-2 my-1" onClick={handlelearClick} >Clear Text</button>
             </div>
             <div className="container my-3" style={{color: props.mode === 'dark'?'white':'#042743'}}>
                 <h2>Your text summary</h2>
                 <p>{text.split(" ").filter((element)=>{return element.length!==0}).length}
                  words and {text.length} characters</p>
-                <p>{0.0008 * text.split(" ").length} minutes read</p>
+                <p>{0.0008 * text.split(" ").filter((element)=>{return element.length!==0}).length} minutes read</p>
                 <h2>Preview</h2>
-                <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
+                <p>{text.length>0?text:"Nothing to preview !"}</p>
             </div>
         </>
     )
